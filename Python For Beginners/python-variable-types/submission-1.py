1c0e237c8be6293f1b80f0e65e90455032f201d7
Jasmeet Singh <67015651+JSM313@users.noreply.github.com>
2026-09-22 18:55:30 +0530

    Add: python-variable-types - submission-1

---
+++ Python For Beginners/python-variable-types/submission-1.py
@@ -0,0 +1,14 @@
+
+integer_type = 2
+float_type = 2.0
+bool_type = False
+string_type = "Hi"
+list_type = []
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
