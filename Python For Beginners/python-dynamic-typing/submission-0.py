fe60f9a3821c09307f212697f768813b818078fe
Jasmeet Singh <67015651+JSM313@users.noreply.github.com>
2026-09-22 18:57:42 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.0
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Hello"
+print(type(variable))
+variable = ["Apple", "Mango", "Orange"]
+print(type(variable))
