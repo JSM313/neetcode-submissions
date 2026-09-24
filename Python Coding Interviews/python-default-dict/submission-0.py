b3fe20889dbd61848edf6f95df4ad6d403f9bfc7
Jasmeet Singh <67015651+JSM313@users.noreply.github.com>
2026-09-24 20:59:00 +0530

    Add: python-default-dict - submission-0

---
+++ Python Coding Interviews/python-default-dict/submission-0.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+from typing import List, Dict
+
+
+def count_chars(s: str) -> Dict[str, int]:
+    frequency = defaultdict(int)
+    for ch in s:
+        if ch in frequency:
+            frequency[ch] += 1
+        else:
+            frequency[ch] = 1
+    return frequency
+
+
+def nested_list_to_dict(nums: List[List[int]]) -> Dict[int, List[int]]:
+    nested_dict = defaultdict(list)
+    for num in nums:
+        nested_dict[num[0]] += num[1::]
+    return nested_dict
+
+
+
+# do not modify below this line
+print(count_chars("hello"))
+print(count_chars("helloworld"))
+print(count_chars("areallylongstringwhyareyoureadingthishahalol"))
+
+print(nested_list_to_dict([[1, 2, 3], [4, 5, 6], [1, 4]]))
+print(nested_list_to_dict([[1, 2, 3, 4], [4, 5, 6, 7], [1, 4, 5, 6]]))
+print(nested_list_to_dict([[5, 2, 3, 4, 5], [4, 5, 6, 7, 8], [5, 6, 7, 8, 9]]))
+print(nested_list_to_dict([[3, 2, 3, 4, 5], [4, 5, 6, 7, 8], [5, 6, 7, 8]]))
